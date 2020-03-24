cb1712585d794d8a88851ad9060b0f316142f46e
Mateusz Midor <3demaniac@gmail.com>
2020-03-24 12:31:43 +0100

    Added e12-wolumeny

---
+++ KubernetesPoPolsku/s01/e12-wolumeny/README.md
@@ -0,0 +1,3 @@
+# Przechowywanie danych na wolumenach
+
+https://www.youtube.com/watch?v=HtWZ8XodqUc&list=PLC2hWv6J_iIzt3140dXL-Ts31Owodl7lB&index=12
---
+++ KubernetesPoPolsku/s01/e12-wolumeny/hardcoded/run_all.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+trap tearDown SIGINT
+
+POD_NAME="hostpath-pod"
+
+function stage() {
+    GREEN="\e[92m"
+    RESET="\e[0m"
+    msg="$1"
+    
+    echo
+    echo -e "$GREEN$msg$RESET"
+}
+
+function waitPodStatus() {
+    pod_name="$1"
+    pod_status="$2"
+    stage "Waiting $pod_name is $pod_status"
+
+    while [[ `sudo kubectl get pod | grep $pod_name | grep $pod_status` == "" ]]; do sleep 1; done
+    echo "OK"
+}
+
+function checkPrerequsites() {
+    stage "Checking prerequisites"
+
+    command minikube > /dev/null 2>&1
+    [[ $? != 0 ]] && echo "You need to install minicube to run local cluster" && exit 1
+
+    echo "OK"
+}
+
+function runMinikube() {
+    stage "Running Minikube"
+
+    desired_status=": Running : Running : Running : Configured "
+    if [[ `sudo minikube status | egrep -o ":.*" | tr '\n' ' '` != $desired_status ]]; then
+        sudo rm -f /tmp/juju-mk*
+        sudo minikube stop
+        sudo rm -f /tmp/juju-mk*
+        echo "Running minikube"
+        sudo minikube start --vm-driver=none
+    else
+        echo "Minikube is running"
+    fi
+
+    ip=`sudo minikube ip`
+    echo "Your ClusterIP: $ip"
+}
+
+function runExample() {
+    stage "Runnig pod"
+
+    sudo kubectl apply -f objects.yml
+    waitPodStatus $POD_NAME "Completed"
+}
+
+function showLogs() {
+    stage "Printing pod logs"
+    
+    sudo kubectl logs $POD_NAME -f
+}
+
+function keepAlive() {
+    stage "CTRL+C to exit"
+
+    while true; do sleep 1; done
+}
+
+function tearDown() {
+    sudo kubectl delete pod $POD_NAME
+    echo "Done."
+    exit 0
+}
+
+checkPrerequsites
+runMinikube
+
+runExample
+showLogs
+keepAlive
