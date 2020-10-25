ac9a3afdb4a5341042e7b0991444a21629b4afab
Mateusz Midor <3demaniac@gmail.com>
2020-10-25 15:16:52 +0100

    Updated s01/e01

--- Kubernetes/KubernetesPoPolsku/s01/e01-uruchamiamy-minikube/run_all.sh
+++ Kubernetes/KubernetesPoPolsku/s01/e01-uruchamiamy-minikube/run_all.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/env bash
 
+function stage() {
+    BOLD_BLUE="\e[1m\e[34m"
+    RESET="\e[0m"
+    msg="$1"
+    
+    echo
+    echo -e "$BOLD_BLUE$msg$RESET"
+}
 
-function installMinikubeArchLinux() {
+function checkPrerequsites() {
+    stage "Checking prerequisites"
+
     command minikube > /dev/null 2>&1
-    if [[ $? != 0 ]]; then
-        echo "Installing minikube"
-        pamac build minikube-bin
-        [[ $? != 0 ]] && echo "Installing minikube failed" && exit 1
-    else
-        echo "Minikube is installed"
-    fi
+    [[ $? != 0 ]] && echo "You need to install minicube to run local cluster" && exit 1
+
+    echo "Done"
 }
 
 function runMinikube() {
-    desired_status=": Running : Running : Running : Configured "
-    if [[ `sudo minikube status | egrep -o ":.*" | tr '\n' ' '` != $desired_status ]]; then
-        sudo rm -f /tmp/juju-mk*
-        sudo minikube stop
-        sudo rm -f /tmp/juju-mk*
-        echo "Running minikube"
-        sudo minikube start --vm-driver=none
-    else
-        echo "Minikube is running"
+    stage "Running minikube"
+
+    desired_status=": Control Plane : Running : Running : Running : Configured "
+    if [[ `minikube status | egrep -o ":.*" | tr '\n' ' '` != $desired_status ]]; then
+        minikube stop
+        minikube start
+        [[ $? != 0 ]] && echo "Running minikube failed" && exit 1
     fi
 
-    ip=`sudo minikube ip`
-    echo "Your ClusterIP: $ip"
+    echo "Done"
 }
 
 function showDashboard() {
-    echo "Running minikube dashboard"
-    sudo minikube dashboard &
+    stage "Running minikube dashboard"
+    
+    minikube dashboard &
     
     url=""
     while true; do
-        url=`sudo kubectl describe service kubernetes-dashboard --namespace kubernetes-dashboard | sed -n '/Endpoints/ s|.* || p'`
+        url=`kubectl describe service kubernetes-dashboard --namespace kubernetes-dashboard | sed -n '/Endpoints/ s|.* || p'`
         curl -s > /dev/null 2>&1 $url
         [[ $? == 0 ]] && break
         echo "Waiting for minikube dashboard..."
@@ -42,8 +46,10 @@ function showDashboard() {
     done
     echo "Your Dashboard URL: $url"
     firefox $url
+
+    echo "Done"
 }
 
-installMinikubeArchLinux
+checkPrerequsites
 runMinikube
 showDashboard
