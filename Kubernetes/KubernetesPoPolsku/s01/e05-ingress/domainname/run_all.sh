ea80c054f7f600580a5f3a5156f64b34c9028cd0
Mateusz Midor <3demaniac@gmail.com>
2020-10-26 19:21:59 +0100

    Updated s01/e05

--- Kubernetes/KubernetesPoPolsku/s01/e05-ingress/domainname/run_all.sh
+++ Kubernetes/KubernetesPoPolsku/s01/e05-ingress/domainname/run_all.sh
@@ -6,73 +6,115 @@ REPLICASET_NAME="hello-world-replicaset"
 SERVICE_NAME="hello-world-service"
 INGRESS_NAME="hello-world-ingress"
 
+function stage() {
+    BOLD_BLUE="\e[1m\e[34m"
+    RESET="\e[0m"
+    msg="$1"
+    
+    echo
+    echo -e "$BOLD_BLUE$msg$RESET"
+}
+
 function checkPrerequsites() {
+    stage "Checking prerequisites"
+
     command minikube > /dev/null 2>&1
     [[ $? != 0 ]] && echo "You need to install minicube to run local cluster" && exit 1
+
+    echo "Done"
 }
 
 function runMinikube() {
-    desired_status=": Running : Running : Running : Configured "
-    if [[ `sudo minikube status | egrep -o ":.*" | tr '\n' ' '` != $desired_status ]]; then
-        sudo rm -f /tmp/juju-mk*
-        sudo minikube stop
-        sudo rm -f /tmp/juju-mk*
-        echo "Running minikube"
-        sudo minikube start --vm-driver=none
-    else
-        echo "Minikube is running"
+    stage "Running minikube"
+
+    host_status=`minikube status -f '{{ .Host }}'`
+    kubelet_status=`minikube status -f '{{ .Kubelet }}'`
+    apiserver_status=`minikube status -f '{{ .APIServer }}'`
+    if [ $host_status != "Running"  ] || [ $kubelet_status != "Running"  ] || [ $apiserver_status != "Running"  ]; then
+        minikube stop
+        minikube start
+        [[ $? != 0 ]] && echo "Running minikube failed" && exit 1
     fi
 
-    ip=`sudo minikube ip`
-    echo "Your ClusterIP: $ip"
+    echo "Done"
 }
 
 function configIngress() {
-    if [[ `sudo minikube addons list | grep ingress | grep enabled` == "" ]]; then
+    stage "Configuring Ingress minikube addon"
+
+    if [[ `minikube addons list | grep ingress | grep enabled` == "" ]]; then
         echo "Minikube ingress is not running. Run now..."
-        sudo minikube addons enable ingress
-        echo "Done."
-    else
-        echo "Minikube ingress is running"
+        minikube addons enable ingress
     fi
+
+    echo "Done"
 }
 
-function runAll() {
-    echo "Running ReplicaSet, Service and Ingress"
-    sudo kubectl apply -f deployment.yml
+function createReplicaSetServiceIngress() {
+    stage "Creating ReplicaSet, Service and Ingress"
 
-    # wait replicaset is up
-    while [[ `sudo kubectl get replicaset | grep $REPLICASET_NAME` == "" ]]; do sleep 1; done
+    kubectl apply -f replicaset_service_ingress.yml
 
-    # wait pod is up. Pod name is replicaset name + some id
-    while [[ `sudo kubectl get pods | grep $REPLICASET_NAME | grep Running` == "" ]]; do sleep 1; done
+    # wait ReplicaSet is up
+    while [[ `kubectl get replicaset $REPLICASET_NAME -o jsonpath='{ .status.readyReplicas }'` != "3" ]]; do 
+        echo "waiting..."; 
+        sleep 1; 
+    done
 
     # wait ingress is up
-    while [[ `sudo kubectl get ingress | grep $INGRESS_NAME` == "" ]]; do sleep 1; done
+    while [[ `kubectl get ingress $INGRESS_NAME -o jsonpath='{ .kind }'` != "Ingress" ]]; do 
+        echo "waiting..."; 
+        sleep 1; 
+    done
+
+    echo "Done"
+}
+
+function patchIngressHost() {
+    stage "Patching ingress host to minikube ip"
+
+    # the ingress host IP part must be minikube IP
+    host="hello-world.`minikube ip`.nip.io"
+    kubectl patch ingress $INGRESS_NAME --type='json' -p='[{"op": "replace", "path": "/spec/rules/0/host", "value":"'$host'"}]'
+
+    echo "Done"
 }
 
 function showWebPage() {
-    url="hello-world.127.0.0.1.nip.io" # defined in deployment.yml
+    stage "Showing web page"
+
+    url="hello-world.`minikube ip`.nip.io"
 
     # wait till http server is up
     echo "Waiting for http server to get up"
     while true; do curl --max-time 1 $url > /dev/null 2>&1; [[ $? == 0 ]] && break; sleep 1; done
-
     sleep 3
     firefox $url
     
+    echo "Done"
+}
+
+function keepAlive() {
+    stage "CTRL+C to exit"
+
     while true; do sleep 1; done
 }
 
 function tearDown() {
-    sudo kubectl delete ingress $INGRESS_NAME
-    sudo kubectl delete service $SERVICE_NAME
-    sudo kubectl delete replicaset $REPLICASET_NAME
+    stage "Tear down"
+
+    kubectl delete ingress $INGRESS_NAME
+    kubectl delete service $SERVICE_NAME
+    kubectl delete replicaset $REPLICASET_NAME
+
+    echo "Done"
     exit 0
 }
 
 checkPrerequsites
 runMinikube
 configIngress
-runAll
+createReplicaSetServiceIngress
+patchIngressHost
 showWebPage
+keepAlive
