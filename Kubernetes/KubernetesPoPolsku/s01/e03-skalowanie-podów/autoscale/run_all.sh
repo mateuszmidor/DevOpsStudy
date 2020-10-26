75a9c02d2a918a4a84a919e880056a5a851644ed
Mateusz Midor <3demaniac@gmail.com>
2020-10-26 16:55:13 +0100

    Updated s01/e03

--- Kubernetes/KubernetesPoPolsku/s01/e03-skalowanie-podów/autoscale/run_all.sh
+++ Kubernetes/KubernetesPoPolsku/s01/e03-skalowanie-podów/autoscale/run_all.sh
@@ -5,66 +5,88 @@ trap tearDown SIGINT
 HPA_NAME="hello-world-hpa"
 REPLICASET_NAME="hello-world-replicaset"
 
+function stage() {
+    BOLD_BLUE="\e[1m\e[34m"
+    RESET="\e[0m"
+    msg="$1"
+    
+    echo
+    echo -e "$BOLD_BLUE$msg$RESET"
+}
+
 function checkPrerequsites() {
+    stage "Checking prerequisites"
+
     command minikube > /dev/null 2>&1
     [[ $? != 0 ]] && echo "You need to install minicube to run local cluster" && exit 1
+
+    echo "Done"
 }
 
 function runMinikube() {
-    desired_status=": Running : Running : Running : Configured "
-    if [[ `sudo minikube status | egrep -o ":.*" | tr '\n' ' '` != $desired_status ]]; then
-        sudo rm -f /tmp/juju-mk*
-        sudo minikube stop
-        sudo rm -f /tmp/juju-mk*
-        echo "Running minikube"
-        sudo minikube start --vm-driver=none
-    else
-        echo "Minikube is running"
-    fi
+    stage "Running minikube with pod autoscaling delay set to 15 seconds"
 
-    ip=`sudo minikube ip`
-    echo "Your ClusterIP: $ip"
+    minikube stop
+    minikube start \
+        — extra-config=controller-manager.horizontal-pod-autoscaler-upscale-delay=15s \
+        — extra-config=controller-manager.horizontal-pod-autoscaler-downscale-delay=15s \
+        — extra-config=controller-manager.horizontal-pod-autoscaler-sync-period=10s \
+        — extra-config=controller-manager.horizontal-pod-autoscaler-downscale-stabilization=1m
+    [[ $? != 0 ]] && echo "Running minikube failed" && exit 1
+
+    echo "Done"
 }
 
 function configMetricsServer() {
-    if [[ `sudo minikube addons list | grep metrics-server | grep enabled` == "" ]]; then
+    stage "Configure metrics server"
+
+    if [[ `minikube addons list | grep metrics-server | grep enabled` == "" ]]; then
         echo "Minikube metrics-server is not running. Run now..."
-        sudo minikube addons enable metrics-server
-        echo "Done. Check in minikube dashboard if metrics are available"
-    else
-        echo "Minikube metrics-server is running"
+        minikube addons enable metrics-server
+        echo "Check in minikube dashboard if metrics are available"
     fi
+
+    echo "Done"
 }
 
-function runReplicaSet() {
-    echo "Running ReplicaSet"
-    sudo kubectl apply -f deployment.yml
+function createReplicaSet() {
+    stage "Creating ReplicaSet and Horizontal Pod Autoscaler"
+    
+    kubectl apply -f replicaset.yml
 
-    # wait replicaset is up
-    while [[ `sudo kubectl get replicaset | grep $REPLICASET_NAME` == "" ]]; do sleep 1; done
-
-    # wait pod is up. Pod name is replicaset name + some id
-    while [[ `sudo kubectl get pods | grep $REPLICASET_NAME | grep Running` == "" ]]; do sleep 1; done
+    # wait ReplicaSet is up
+    while [[ `kubectl get replicaset $REPLICASET_NAME -o jsonpath='{ .status.readyReplicas }'` != "1" ]]; do 
+        echo "waiting..."; 
+        sleep 1; 
+    done
+    
+    echo "Done"
 }
 
 function autoscaleReplicaSet() {
-    echo "Waiting for autoscale with max num replicase = 3 based on cpu usage > 50%. It can take a few minutes"
+    stage "Autoscale ReplicaSet"
+
+    echo "Waiting for autoscale with max num replicase = 3 based on cpu usage vs requested cpu > 50%. It can take a few minutes"
 
     while true; do 
-        sudo kubectl get hpa
-        echo
+        kubectl get hpa
         sleep 3
+        echo
     done
 }
 
 function tearDown() {
-    sudo kubectl delete hpa $HPA_NAME
-    sudo kubectl delete replicaset $REPLICASET_NAME
+    stage "Tear down"
+
+    kubectl delete hpa $HPA_NAME
+    kubectl delete replicaset $REPLICASET_NAME
+
+    echo "Done"
     exit 0
 }
 
 checkPrerequsites
 runMinikube
 configMetricsServer
-runReplicaSet
+createReplicaSet
 autoscaleReplicaSet
--- Kubernetes/KubernetesPoPolsku/s01/e03-skalowanie-podów/scale/run_all.sh
+++ Kubernetes/KubernetesPoPolsku/s01/e03-skalowanie-podów/scale/run_all.sh
@@ -4,57 +4,87 @@ trap tearDown SIGINT
 
 REPLICASET_NAME="hello-world-replicaset"
 
+function stage() {
+    BOLD_BLUE="\e[1m\e[34m"
+    RESET="\e[0m"
+    msg="$1"
+    
+    echo
+    echo -e "$BOLD_BLUE$msg$RESET"
+}
+
 function checkPrerequsites() {
+    stage "Checking prerequisites"
+
     command minikube > /dev/null 2>&1
     [[ $? != 0 ]] && echo "You need to install minicube to run local cluster" && exit 1
+
+    echo "Done"
 }
 
 function runMinikube() {
-    desired_status=": Running : Running : Running : Configured "
-    if [[ `sudo minikube status | egrep -o ":.*" | tr '\n' ' '` != $desired_status ]]; then
-        sudo rm -f /tmp/juju-mk*
-        sudo minikube stop
-        sudo rm -f /tmp/juju-mk*
-        echo "Running minikube"
-        sudo minikube start --vm-driver=none
-    else
-        echo "Minikube is running"
+    stage "Running minikube"
+
+    host_status=`minikube status -f '{{ .Host }}'`
+    kubelet_status=`minikube status -f '{{ .Kubelet }}'`
+    apiserver_status=`minikube status -f '{{ .APIServer }}'`
+    if [ $host_status != "Running"  ] || [ $kubelet_status != "Running"  ] || [ $apiserver_status != "Running"  ]; then
+        minikube stop
+        minikube start
+        [[ $? != 0 ]] && echo "Running minikube failed" && exit 1
     fi
 
-    ip=`sudo minikube ip`
-    echo "Your ClusterIP: $ip"
+    echo "Done"
 }
 
-function runReplicaSet() {
-    echo "Running ReplicaSet"
-    sudo kubectl apply -f deployment.yml
+function createReplicaSet() {
+    stage "Creating ReplicaSet"
 
-    # wait replicaset is up
-    while [[ `sudo kubectl get replicaset | grep $REPLICASET_NAME` == "" ]]; do sleep 1; done
+    kubectl apply -f replicaset.yml
 
-    # wait pod is up. Pod name is replicaset name + some id
-    while [[ `sudo kubectl get pods | grep $REPLICASET_NAME | grep Running` == "" ]]; do sleep 1; done
+    # wait ReplicaSet is up
+    while [[ `kubectl get replicaset $REPLICASET_NAME -o jsonpath='{ .status.readyReplicas }'` != "1" ]]; do 
+        echo "waiting..."; 
+        sleep 1; 
+    done
+    
+    echo "Done"
 }
 
 function scaleReplicaSet() {
+    stage "Scale ReplicaSet 1 -> 7"
+
     echo "Current num replicase = 1"
-    sudo kubectl get replicaset
+    kubectl get replicaset
 
     echo "Scaling num replicase to 7 and waiting for 7 pods..."
-    sudo kubectl scale replicaset $REPLICASET_NAME --replicas=7
-    while [[ `sudo kubectl get replicaset | grep $REPLICASET_NAME | sed 's|\s\s*| |g' | cut -d ' ' -f4` != "7" ]]; do sleep 1; done
-    sudo kubectl get replicaset
+    kubectl scale replicaset $REPLICASET_NAME --replicas=7
+    while [[ `kubectl get replicaset $REPLICASET_NAME -o jsonpath='{ .status.readyReplicas }'` != "7" ]]; do 
+        echo "waiting..."; 
+        sleep 1; 
+    done
+    kubectl get replicaset
+
     echo "Done"
+}
+
+function keepAlive() {
+    stage "CTRL+C to exit"
 
     while true; do sleep 1; done
 }
 
 function tearDown() {
-    sudo kubectl delete replicaset $REPLICASET_NAME
+    stage "Tear down"
+
+    kubectl delete replicaset $REPLICASET_NAME
+
+    echo "Done"
     exit 0
 }
 
 checkPrerequsites
 runMinikube
-runReplicaSet
+createReplicaSet
 scaleReplicaSet
+keepAlive
