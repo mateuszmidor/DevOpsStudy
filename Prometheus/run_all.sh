1dcaf74faf350f96e4db46156cd66ec59279a865
Mateusz Midor <3demaniac@gmail.com>
2020-12-12 17:23:33 +0100

    Added Prometheus

---
+++ Prometheus/run_all.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+trap tearDown SIGINT
+
+function stage() {
+    BOLD_ORANGE="\e[1m\e[33m"
+    RESET="\e[0m"
+    msg="$1"
+    
+    echo
+    echo -e "$BOLD_ORANGE$msg$RESET"
+}
+
+function checkPrerequsites() {
+    stage "Checking prerequisites"
+
+    command docker --version > /dev/null 2>&1
+    [[ $? != 0 ]] && echo "You need to install docker to run this example" && exit 1
+
+    command netcat --version > /dev/null 2>&1
+    [[ $? != 0 ]] && echo "You need to install netcat to run this example" && exit 1
+
+    echo "Done"
+}
+
+function runPrometheus() {
+    stage "Running Prometheus"
+
+    docker run -d --rm --name=my_prometheus --net=host -v `pwd`:/home  prom/prometheus --config.file="/home/config.yml"
+
+    echo "Find your metric under 'wave' name!!!"
+    sleep 3
+    firefox localhost:9090/targets # Prometheus dashboard
+    
+    echo "Done"
+}
+
+function runMetricProvider() {
+    stage "Running 'wave' metric(a sinus function) provider at port 8080"
+
+    SINUS="0.000000000000000 0.2588115870535327 0.49998662654663256 0.7070904020014415 0.8660099611064447 0.9659158336885827 0.999999998926914 0.9659398135111947 0.866056286662226 0.7071559164654717 0.5000668654782968 0.2589010826229296 0.0000000826229296 -0.25872208926231904 -0.49990638332273907 -0.7070248814672611 -0.8659636281162382 -0.965891845573885 -0.9999999903422263 -0.9659637850415149 -0.8661026047831848 -0.7072214248587892 -0.5001471001170429 -0.25899057596974184"
+    HEADER="HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Encoding: identity\nServer: golang\nContent-Length: 19\nConnection: close" # Content-Lenght is important
+   
+    while true; do
+        for s in $SINUS; do
+            printf "$HEADER\n\nwave $s" | netcat  -l -p 8080 localhost
+        done
+    done
+
+    echo "Done"
+}
+
+
+function tearDown() {
+    stage "Tear down"
+
+    docker stop my_prometheus
+
+    echo "Done"
+    exit 0
+}
+
+function keepAlive() {
+    stage "CTRL+C to exit"
+
+    while true; do sleep 1; done
+}
+
+checkPrerequsites
+runPrometheus
+runMetricProvider
+displayPrometheusWeb
+keepAlive
